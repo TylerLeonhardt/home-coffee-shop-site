bab7fd4d13e64cc161876e1b7cbc3c11f88290a6
Tyler James Leonhardt <me@tylerleonhardt.com>
2025-01-16 23:25:17 -0800

    Fix site

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
-import ReactDOM from "https://esm.sh/react-dom@18.2.0";
-import { html } from "https://esm.sh/htm@3.1.1/react";
+import React from "https://esm.sh/react@18.3.1";
+import ReactDOM from "https://esm.sh/react-dom@18.3.1";
+import htm from "https://esm.sh/htm@3.1.1";
 import menu from './data/menu.js';
+
+const html = htm.bind(React.createElement);
 
 const Logo = () => {
   return html`
